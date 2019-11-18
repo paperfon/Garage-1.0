e0280919a605f83f5ec5256dc1308507378511b4
Rod Paperfon <admin@paperfon.com>
2019-11-18 16:25:19 +0100

    WIP: Tests

--- Garage 1.0.Tests/GarageTests.cs
+++ Garage 1.0.Tests/GarageTests.cs
@@ -6,7 +6,7 @@ namespace Garage_1._0.Tests
     public class GarageTests
     {
         [Fact]
-        public void Garage_NewInstance()
+        public void Garage_NewInstance_CreatedWithNameAndCapacity()
         {
             // Arrange
             var expectedName = "My Sharona";
@@ -20,6 +20,18 @@ namespace Garage_1._0.Tests
             // Assert
             Assert.Equal(expectedName, actualName);
             Assert.Equal(expectedCapacity, actualCapacity);
+        }
+
+        [Fact]
+        public void ListParkVehicles_WhenEmpty_ReturnsNull()
+        {
+            //var expected = (string[])null;
+            //Vehicle[] expected = null;
+            // https://stackoverflow.com/questions/6802573/interfaces-whats-the-point
+            var handler = new GarageHandler();
+            var garage = new Garage<Vehicle>("Abcd", 4);
+
+            handler.ListParkedVehicles(garage);
         }
     }
 }
